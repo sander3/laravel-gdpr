fdf6c94e66d43a7b82df7255bea2c7bdab71bef4
Sander de Vos <sander@tutanota.de>
2018-05-04 17:04:52 +0200

    WIP: GDPR compliant data portability

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('test', function () {
-    return 'Hello world';
+    return auth()->user()->portable();
 });
---
+++ src/Portable.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Soved\Laravel\Gdpr;
+
+trait Portable
+{
+    /**
+     * Convert the model instance to an GDPR compliant data portability array.
+     *
+     * @return array
+     */
+    public function portable()
+    {
+        // Eager load the given relations
+        if (isset($this->gdprWith)) {
+            $this->loadMissing($this->gdprWith);
+        }
+
+        // Make the given attributes visible
+        if (isset($this->gdprVisible)) {
+            $this->makeVisible($this->gdprVisible);
+        }
+
+        // Make the given attributes hidden
+        if (isset($this->gdprHidden)) {
+            $this->makeHidden($this->gdprHidden);
+        }
+
+        return $this->toPortableArray();
+    }
+
+    /**
+     * Get the GDPR compliant data portability array for the model.
+     *
+     * @return array
+     */
+    public function toPortableArray()
+    {
+        return $this->toArray();
+    }
+}
