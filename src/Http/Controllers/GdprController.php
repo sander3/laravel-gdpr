c887f535e4b0cd245a85612cad2b394923f1f14a
Sander de Vos <sander@tutanota.de>
2018-06-15 15:19:46 +0200

    Resolves #11

--- src/Http/Controllers/GdprController.php
+++ src/Http/Controllers/GdprController.php
@@ -4,6 +4,7 @@ namespace Soved\Laravel\Gdpr\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Http\FormRequest;
 use Soved\Laravel\Gdpr\Events\GdprDownloaded;
 use Soved\Laravel\Gdpr\Http\Requests\GdprDownload;
 
@@ -17,12 +18,9 @@ class GdprController extends Controller
      */
     public function download(GdprDownload $request)
     {
-        $credentials = [
-            $request->user()->getAuthIdentifierName() => $request->user()->getAuthIdentifier(),
-            'password'                                => $request->input('password'),
-        ];
-
-        abort_unless(Auth::attempt($credentials), 403);
+        if (!$this->attemptLogin($request)) {
+            return $this->sendFailedLoginResponse();
+        }
 
         $data = $request->user()->portable();
 
@@ -37,5 +35,31 @@ class GdprController extends Controller
                 'Content-Disposition' => 'attachment; filename="user.json"',
             ]
         );
+    }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Foundation\Http\FormRequest  $request
+     * @return bool
+     */
+    protected function attemptLogin(FormRequest $request)
+    {
+        $credentials = [
+            $request->user()->getAuthIdentifierName() => $request->user()->getAuthIdentifier(),
+            'password'                                => $request->input('password'),
+        ];
+
+        return Auth::attempt($credentials);
+    }
+
+    /**
+     * Get the failed login response.
+     *
+     * @return void
+     */
+    protected function sendFailedLoginResponse()
+    {
+        abort(403, 'Unauthorized.');
     }
 }
